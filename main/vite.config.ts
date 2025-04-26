69a5c74f365e8bc12a440ca487fe7ca2bfb644b3
Kevin Kim <kk946@cornell.edu>
2025-04-26 16:37:56 +0900

    Update vite.config.ts

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/games4cause.github.io/",
   plugins: [tailwindcss()],
 });
