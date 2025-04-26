a498352ea3e375aea988f1465587b2599b6a7878
Kevin Kim <kk946@cornell.edu>
2025-04-26 17:12:26 +0900

    Update App.tsx

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,16 +1,15 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
-
 
 export default App;
