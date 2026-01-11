dcedb0a45152b10996931c2ed236ec134b45ae2b
Kevin Kim <kk946@cornell.edu>
2026-01-11 18:32:58 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import { ThemeProvider } from "./components/ThemeProvider";
 import Contribute from "./pages/Contribute";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen min-h-[100dvh]">
+          <Navbar />
+          <main className="flex-1 flex items-center justify-center">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contribute" element={<Contribute />} />
+            </Routes>
+          </main>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contribute" element={<Contribute />} />
-          </Routes>
         </div>
       </ThemeProvider>
     </BrowserRouter>
